60c07423ea8f9a5f4dee762588ab6973aceaf175
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-04-05 22:01:10 -0500

    working in fullscreen and zoomRange

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MapsRoutingModule } from './maps-routing.module';
-import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
-import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { MapsRoutingModule } from './maps-routing.module';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+
+import * as mapboxgl from 'mapbox-gl';
+
+(mapboxgl as any).accessToken= 'pk.eyJ1IjoiaWFkaWF6IiwiYSI6ImNsdWp6cGtiZzBnOWwybG82dThhNzg4eGsifQ.ReBNXF9AuEXaf96QH4y5Vg';
+
+
 
 
 @NgModule({
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,7 +1,6 @@
-import { AfterContentInit, AfterViewInit, Component } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
-(mapboxgl as any).accessToken= 'pk.eyJ1IjoiaWFkaWF6IiwiYSI6ImNsdWp6cGtiZzBnOWwybG82dThhNzg4eGsifQ.ReBNXF9AuEXaf96QH4y5Vg';
 
 
 @Component({
@@ -9,10 +8,13 @@ import * as mapboxgl from 'mapbox-gl';
   styleUrls: ['./full-screen-page.component.css']
 })
 export class FullScreenPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
 
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+
+    if (!this.divMap) throw('El elemento HTML no fué encontrado')
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
